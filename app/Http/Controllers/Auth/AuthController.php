79b7f4316cafaa2d13364f8a95e2c0c32db973ce
ddsbrddn <ddsbrddn@gmail.com>
2019-10-09 12:45:31 +0700

    initial commit

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|unique:users,email|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return new UserResource($user);
+    }
+}
